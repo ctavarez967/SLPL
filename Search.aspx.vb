1c446a25f086ebf94cb3560bddc5cde666f047ef
Vinnie Pizzo <vincent-pizzo@uiowa.edu>
2014-12-07 15:44:20 -0600

    Added functionality to search page

--- Search.aspx.vb
+++ Search.aspx.vb
@@ -2,4 +2,19 @@
 Partial Class Search
     Inherits System.Web.UI.Page
 
+    Protected Sub tb_Title_TextChanged(sender As Object, e As EventArgs) Handles tb_Title.TextChanged
+        Dim searchword As String
+
+        searchword = "Select * From Project_Materials where (Title Like '%" + tb_Title.Text.ToString() + "%')"
+
+        SqlDataSource1.SelectCommand = searchword
+    End Sub
+
+    Protected Sub tb_Author_TextChanged(sender As Object, e As EventArgs) Handles tb_Author.TextChanged
+        Dim searchword As String
+
+        searchword = "Select * From Project_Materials where (Author Like '%" + tb_Author.Text.ToString() + "%')"
+
+        SqlDataSource1.SelectCommand = searchword
+    End Sub
 End Class
